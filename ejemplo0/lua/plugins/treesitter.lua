47cf9482a765657231dc528c7a6139c9d92a517b
Rayán <rbelhajjannus@cifpfbmoll.eu>
2025-11-28 15:56:11 +0100

    Add nvim-treesitter configuration
    Set up nvim-treesitter with specified languages for installation.

---
+++ ejemplo0/lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+require('lazy').setup({
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  branch = 'main',
+  build = ':TSUpdate',
+  config = function()
+     require('nvim-treesitter').install({ 'lua', 'html', 'css', 'javascript', 'typescript', 'angular', 'php', 'java', 'properties', 'dockerfile', 'bash', 'markdown'}) -- Lista de lenguajes de nuestro interes
+  end
+})
